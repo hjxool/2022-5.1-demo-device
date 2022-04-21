5a4ba75fbfb6057b00b8fec5825b673f3f606c12
hjxool <691638334@qq.com>
2022-04-21 18:00:49 +0800

    update

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 let config = new Vue({
 	el: '#config',
 	data: {
-		menu_select: 1, //导航栏选中目标
-		page_loading: false, //页面加载时遮罩
+		static_param: {
+			menu_select: 1, //导航栏选中目标
+			page_loading: false, //页面加载时遮罩
+			first_load: true, //第一次加载显示统计图
+		},
 	},
 	methods: {
 		// 传递选中索引
 		menuSelected: function (index, path) {
-			this.menu_select = Number(path[0]);
+			this.static_param.menu_select = Number(path[0]);
 		},
 	},
 });
